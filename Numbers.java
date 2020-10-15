d5e2b39bd303f9dbec922ee4bbc85496733d0900
Jorden Dave <jordendave@Jordens-MacBook-Pro.local>
2020-10-14 20:00:23 -0500

    homework

---
+++ Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+
+public class Numbers {
+    public static void main(String[] args) 
+    {
+
+    Scanner myObj = new Scanner(System.in);  
+
+    System.out.println("Enter 5 numbers"); 
+    
+}
